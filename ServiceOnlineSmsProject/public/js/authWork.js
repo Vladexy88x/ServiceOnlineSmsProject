57ce1eb5c92c4da4ce1714c3b6849692846c449e
Vladexy88 <jv69986998@gmail.com>
2021-09-22 19:51:38 +0300

    Update authWork.js

--- ServiceOnlineSmsProject/public/js/authWork.js
+++ ServiceOnlineSmsProject/public/js/authWork.js
@@ -1,82 +1,76 @@
 // import fetch from "node-fetch";
 
 function getUserName() {
-    let userNameElement = document.getElementById('username-lk');
-    let balanceElement = document.getElementById('balance-lk');
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', '/localdata', false);
-    //ассинхронное исполнение
-    // xhr.onreadystatechange = function () {
-    //     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    //         console.log(xhr.responseText);
-    //         content = JSON.parse(xhr.responseText);
-    //     };
-    // };
-    xhr.onerror = (e) => {
-        console.log("Error " + e.target.status + " check document");
-    }
-    xhr.send(null);
-    if (xhr.status == 200) {
-        console.log("Запрос на сервер прошел успешно");
-        let data = JSON.parse(xhr.responseText);
-        userNameElement.innerText = `Пользователь: ${data.username}`;
-        balanceElement.innerText = `Баланс:  ${data.balance} рублей`;
-    }
+  const userNameElement = document.getElementById('username-lk');
+  const balanceElement = document.getElementById('balance-lk');
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '/localdata', false);
+  // ассинхронное исполнение
+  // xhr.onreadystatechange = function () {
+  //     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+  //         console.log(xhr.responseText);
+  //         content = JSON.parse(xhr.responseText);
+  //     };
+  // };
+  xhr.onerror = (e) => {
+    console.log(`Error ${e.target.status} check document`);
+  };
+  xhr.send(null);
+  if (xhr.status == 200) {
+    console.log('Запрос на сервер прошел успешно');
+    const data = JSON.parse(xhr.responseText);
+    userNameElement.innerText = `Пользователь: ${data.username}`;
+    balanceElement.innerText = `Баланс:  ${data.balance} рублей`;
+  }
 }
 
 function authRelease() {
+  getUserName();
+
+  // чтобы работала window.location нужно в кнопке поставить тип button
+  // window.location = '/content';
 
 
-    getUserName();
+  // fetch('/checkauth?login=sss&passwordJ=2432', {
+  //     method: "GET",
 
-    //чтобы работала window.location нужно в кнопке поставить тип button
-    //window.location = '/content';
+  // })
+  //     .then(res => res.json())
+  //     .then((result) => {
+  //         console.log(result);
+  //     });
 
-
-    // fetch('/checkauth?login=sss&passwordJ=2432', {
-    //     method: "GET",
-
-    // })
-    //     .then(res => res.json())
-    //     .then((result) => {
-    //         console.log(result);
-    //     });
-
-    // fetch('/checkauth', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({
-    //         user: {
-    //             login: "Face",
-    //             password: "123"
-    //         }
-    //     })
-    // });
-   
+  // fetch('/checkauth', {
+  //     method: 'GET',
+  //     headers: {
+  //         'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //         user: {
+  //             login: "Face",
+  //             password: "123"
+  //         }
+  //     })
+  // });
 
 
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('GET', `/localname`, false);
-    // //ассинхронное исполнение
-    // // xhr.onreadystatechange = function () {
-    // //     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    // //         console.log(xhr.responseText);
-    // //         content = JSON.parse(xhr.responseText);
-    // //     };
-    // // };
-    // xhr.onerror = (e) => {
-    //     console.log("Error " + e.target.status + " check document");
-    // }
-    // xhr.send(null);
-    // if (xhr.status == 200) {
-    //     console.log("Запрос на авторизацию прошел успешно");
-    //     authUserName.innerText = xhr.responseText;
-    // }
-
-
-
+  // let xhr = new XMLHttpRequest();
+  // xhr.open('GET', `/localname`, false);
+  // //ассинхронное исполнение
+  // // xhr.onreadystatechange = function () {
+  // //     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+  // //         console.log(xhr.responseText);
+  // //         content = JSON.parse(xhr.responseText);
+  // //     };
+  // // };
+  // xhr.onerror = (e) => {
+  //     console.log("Error " + e.target.status + " check document");
+  // }
+  // xhr.send(null);
+  // if (xhr.status == 200) {
+  //     console.log("Запрос на авторизацию прошел успешно");
+  //     authUserName.innerText = xhr.responseText;
+  // }
 }
 
-window.addEventListener("load", authRelease, false);
+window.addEventListener('load', authRelease, false);
