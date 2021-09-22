69f8ec9e1a7e8fba093fb73cc4a3c4603ebd9cdd
Vladexy88 <jv69986998@gmail.com>
2021-09-22 19:45:41 +0300

    Update content.js

--- ServiceOnlineSmsProject/content.js
+++ ServiceOnlineSmsProject/content.js
@@ -1,26 +1,24 @@
 const path = require('path');
 
 const HomePage = (res) => {
-    res.sendFile(path.join(__dirname + '/public' + '/index.html'));
-}
+  res.sendFile(path.join(`${__dirname}/public` + '/index.html'));
+};
 
 const AuthPage = (res) => {
-    res.sendFile(path.join(__dirname  + '/public' + '/authorization.html'));
-    console.log(__dirname);
-}
+  res.sendFile(path.join(`${__dirname}/public` + '/authorization.html'));
+  console.log(__dirname);
+};
 
 const RegisterPage = (res) => {
-    res.sendFile(path.join(__dirname + '/public' + '/registration.html'));
-}
+  res.sendFile(path.join(`${__dirname}/public` + '/registration.html'));
+};
 
 const FailedAuthorizationPage = (res) => {
-    res.sendFile(path.join(__dirname + '/public' + '/failedAuthorization.html'));
-}
+  res.sendFile(path.join(`${__dirname}/public` + '/failedAuthorization.html'));
+};
 const PasswordResetPage = (res) => {
-    res.sendFile(path.join(__dirname + '/public' + '/passwordReset.html'));
-}
-
-
+  res.sendFile(path.join(`${__dirname}/public` + '/passwordReset.html'));
+};
 
 module.exports.HomePage = HomePage;
 module.exports.AuthPage = AuthPage;
