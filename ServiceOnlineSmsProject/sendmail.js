ab04786bc0d484bf08fabeda01089c58d719d838
Vladexysys88 <jv69986998@gmail.com>
2022-08-19 23:15:15 +0300

    Update sendmail.js

--- ServiceOnlineSmsProject/sendmail.js
+++ ServiceOnlineSmsProject/sendmail.js
@@ -115,11 +115,7 @@ async function SendMessageMail(email, message) {
   });
 
   console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
   console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 module.exports = SendMessageMail;
